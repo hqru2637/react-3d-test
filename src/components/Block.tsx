bb6b4ec58b5180f503d76b036f6f2fb32b4fb559
Totu2637 <62416195+hqru2637@users.noreply.github.com>
2024-06-27 00:38:42 +0900

    refactor: improve texture loader

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from 'react';
-import { Color, Vector3, useFrame, useLoader } from '@react-three/fiber';
+import { Color, Vector3, useLoader } from '@react-three/fiber';
 import { Mesh } from 'three';
 import { TextureLoader, NearestFilter } from 'three';
 
@@ -17,17 +17,17 @@ type BlockProps = {
 
 export default function Block(props: BlockProps) {
   const mesh = React.useRef<Mesh>(null!);
-  const paths = Object.keys(textures);
-  const loadedTextures = useLoader(TextureLoader, paths.map(k => (textures as Record<string, string>)[k])).map(t => {
-    t.minFilter = t.magFilter = NearestFilter;
-    t.generateMipmaps = false;
-    return t;
-  });
+  const texture = useLoader(TextureLoader, textures[props.texture]);
+  texture.minFilter = texture.magFilter = NearestFilter;
+  texture.generateMipmaps = false;
 
   return (
-    <mesh position={props.position} ref={mesh}>
+    <mesh 
+      position={props.position}
+      ref={mesh}
+    >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial map={loadedTextures[paths.findIndex(p => p === props.texture)]} />
+      <meshStandardMaterial map={texture} />
     </mesh>
   );
 }
