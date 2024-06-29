a886172f4ec23bb1fbdb7964edf6893ee5a43027
Totu2637 <62416195+hqru2637@users.noreply.github.com>
2024-06-29 12:21:06 +0900

    feat: background color, debug stats

--- app/CanvasWrapper.tsx
+++ app/CanvasWrapper.tsx
@@ -1,20 +1,23 @@
 "use client";
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Stats } from '@react-three/drei';
 import Box from '@/components/Box';
 import Block from '@/components/Block';
 import { fillBlocks } from '@/utils';
 
 export default function CanvasWrapper() {
+  
   const blocks = fillBlocks(
-    { x: -3, y: -1, z: -3 },
-    { x: 3, y: -1, z: 3 },
+    { x: -10, y: 0, z: -10 },
+    { x: 10, y: 0, z: 10 },
     'dirt'
   );
 
   return (
-    <Canvas gl={{ antialias: false }}>
+    <Canvas gl={{ antialias: false }} camera={{ fov: 80 }}>
       <OrbitControls />
+      <Stats />
+      <color attach="background" args={['#e0fdff']} />
       <ambientLight />
       <directionalLight />
       <fog attach="fog" color={'#fff'} near={3} far={30} />
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CanvasWrapper from './CanvasWrapper';
 export default function Main() {
   return (
     <main>
-      <div style={{ width: '100vw', height: '100vw' }}>
+      <div style={{ width: '100vw', height: '60vw' }}>
         <CanvasWrapper />
       </div>
     </main>
